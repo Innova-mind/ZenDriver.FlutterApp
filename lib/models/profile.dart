049cc67e7a0b86623b631940db77d0a80f8ac510
Frank_Alva <u202020026@upc.edu.pe>
2023-06-13 15:09:09 -0500

    feat(Profile): Interface

---
+++ lib/models/profile.dart
@@ -0,0 +1,55 @@
+class Profile {
+  final int id;
+  final String firstName;
+  final String lastName;
+  final String userName;
+  final String password;
+  final String phone;
+  final String role;
+  final String description;
+  final String imageUrl;
+  final String birthdayDate;
+
+  Profile({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.userName,
+    required this.password,
+    required this.phone,
+    required this.role,
+    required this.description,
+    required this.imageUrl,
+    required this.birthdayDate,
+  });
+
+  Profile.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        firstName = json['firstName'],
+        lastName = json['lastName'],
+        userName = json['userName'],
+        password = json['password'],
+        phone = json['phone'],
+        role = json['role'],
+        description = json['description'],
+        imageUrl = json['imageUrl'],
+        birthdayDate = json['birthdayDate'];
+
+        
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'firstName': firstName,
+      'lastName': lastName,
+      'userName': userName,
+      'password': password,
+      'phone': phone,
+      'role': role,
+      'description': description,
+      'imageUrl': imageUrl,
+      'birthdayDate': birthdayDate,
+    };
+  }
+
+
+}
