89219a8ddc38a9ba5c4642565040d7479bb3a413
RyanSweden03 <89040278+RyanSweden03@users.noreply.github.com>
2023-06-29 09:53:17 -0500

    style(search_your_driver): changed filter colors

--- lib/ui/pages/search_your_driver.dart
+++ lib/ui/pages/search_your_driver.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:zendriver/ui/pages/filtered_drivers.dart';
+
+import '../../models/profiles.dart';
+import '../../services/profile_service.dart';
 
 class SearchYourDriver extends StatefulWidget {
   const SearchYourDriver({super.key});
@@ -10,8 +14,32 @@ class SearchYourDriver extends StatefulWidget {
 
 class _SearchYourDriverState extends State<SearchYourDriver> {
   final _licenseList = ["AI", "AIIA", "AIIB", "AIIIB", "AIIIA", "AIIIC"];
-
+  userProfile? profile;
   String? selectedlicense = "AI";
+  Future<SharedPreferences>? _prefs;
+  userProfile? user;
+  ProfileService? profileService;
+  int? userId;
+  Color _color = const Color.fromRGBO(5, 53, 87, 1);
+  initialize()async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    userId = prefs.getInt('userId');
+    user = await profileService?.getData(userId!);
+    setState(() {
+      user = user;
+    });
+  }
+
+
+  @override
+  void initState() {
+    _prefs = SharedPreferences.getInstance();
+    profileService = ProfileService();
+    initialize();
+    super.initState();
+
+    
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,21 +51,20 @@ class _SearchYourDriverState extends State<SearchYourDriver> {
           const SizedBox(
             height: 65,
           ),
-          const Column(
+          Column(
             children: [
-              Image(
+              const Image(
                   image: AssetImage('assets/logo.png'), width: 50, height: 50),
-              Text(
+              const Text(
                 'ZenDriver',
                 style: TextStyle(fontSize: 10),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 30,
               ),
               Center(
-                child: Text(
-                  '¿Cual es su chofer ideal?',
-                  style: TextStyle(fontSize: 30),
+                child: Text(user?.role == "recruiter" ?"¿Cúal es su chofer ideal?" : "Conoce a otros choferes!",
+                  style: const TextStyle(fontSize: 30),
                 ),
               ),
             ],
@@ -67,49 +94,60 @@ class _SearchYourDriverState extends State<SearchYourDriver> {
                   });
                 }),
           ),
-          const SizedBox(
-            height: 20,
-          ),
-           ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(100, 50),
-                    backgroundColor: const Color.fromRGBO(0, 150, 136, 1),
-                  ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => FilteredDrivers(
-                            licenseType: selectedlicense!,
-                            operation: 1,
-                            title: "Conductores")));
-              },
-              child: const Text("Mostrar Todos los conductores")),
-
           ButtonBar(
             mainAxisSize: MainAxisSize.min,
             children: [
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(100, 50),
-                    backgroundColor: const Color.fromRGBO(0, 150, 136, 1),
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => FilteredDrivers(
-                                licenseType: selectedlicense!,
-                                operation: 0,
-                                title:
-                                    "Conductores con licencia $selectedlicense")));
-                  },
-                  child: const Text(
-                    'Filtrar ya',
-                    style: TextStyle(fontSize: 18),
-                  )),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(100, 50),
+                      backgroundColor: _color,
+                      textStyle: const TextStyle(
+                        fontSize: 14,
+                      )
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FilteredDrivers(
+                                  licenseType: selectedlicense!,
+                                  operation: 0,
+                                  title:
+                                      "Conductores con licencia $selectedlicense")));
+                    },
+                    child: const Text(
+                      'Filtrar ya',
+                      style: TextStyle(fontSize: 18),
+                    )
+                    ),
+              ),
+                  
             ],
-          )
+          ),
+          Padding(
+             padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+             child: ElevatedButton(
+              
+              style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(100, 60),
+                      backgroundColor: _color,
+                      textStyle: const TextStyle(
+                        fontSize: 14,
+                      )
+                    ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => FilteredDrivers(
+                              licenseType: selectedlicense!,
+                              operation: 1,
+                              title: "Conductores")));
+                },
+                child: const Text("Mostrar Todos los conductores")),
+           ),
         ],
       ),
     ));
